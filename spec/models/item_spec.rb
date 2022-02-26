e7631054dff9ccb98bb6c955bec2ff7016bd9681
Shawna Akiona <shawna.akiona@gmail.com>
2022-02-25 21:01:19 -0800

    models & controllers added

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-require 'rails_helper'
-
 # Test suite for the Item model
 RSpec.describe Item, type: :model do
   # Association test
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-require 'rails_helper'
-
 # Test suite for the Todo model
 RSpec.describe Todo, type: :model do
   # Association test
